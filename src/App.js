04e39ba8e9b0ee06f36742bd48c938f57f72b042
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-06-12 00:29:40 +0430

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React from "react";
 import SeasonDisplay from "./SeasonDisplay";
 
-function App() {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return (
-    <div>
-      <SeasonDisplay />
-    </div>
-  );
+class App extends React.Component {
+  state = { lat: null, errMessage: "" };
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errMessage: "something went wrong" })
+    );
+  }
+  render() {
+    if (this.state.errMessage && !this.state.lat) {
+      return <div>Error: {this.state.errMessage}</div>;
+    }
+    if (this.state.lat && !this.state.errMessage) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading...</div>;
+  }
 }
-
 export default App;
